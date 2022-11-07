af788f00d9ad240efafa0f4a997d74e13edc7844
Sergey <melnikov_s_a@mail.ru>
2022-11-08 00:26:47 +0300

    SumOfParts Finally

---
+++ Example2SumOfParts/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Здравствуйте. Введите число");
+
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+void SumAll(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + (number % 10);
+        number = number / 10;
+    }
+    Console.WriteLine($"Сумма цифр числа равна {sum}");
+}
+
+SumAll(numberN);
