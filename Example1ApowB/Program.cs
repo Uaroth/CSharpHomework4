1ef99f200404a991f7641690b4e3be7121cca7ba
Sergey <melnikov_s_a@mail.ru>
2022-11-07 11:27:47 +0300

    Example1

---
+++ Example1ApowB/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+Console.WriteLine("Здравствуйте. Поочерёдно введите число и степень");
+double numA = Convert.ToDouble(Console.ReadLine());
+double numB = Convert.ToDouble(Console.ReadLine());
+double result = Math.Pow(numA , numB);
+Console.WriteLine($"Число {numA} в степени {numB} = {result}");
