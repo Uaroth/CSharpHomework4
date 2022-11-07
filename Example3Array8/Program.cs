187045e594cf1b81435a2ea1da9e57f4dda9540c
Sergey <melnikov_s_a@mail.ru>
2022-11-08 02:06:46 +0300

    Example3 array8

---
+++ Example3Array8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+
+Console.WriteLine("Ваш случайный массив из 8 элементов готов");
+
+int[] numbers = new int[8];
+
+EightRandoms(numbers);
+
+void EightRandoms(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next();
+        Console.Write($"{numbers[i]} . ");
+    }  
+}
+
+
